093426ffb7c41dab5c2e241a02591ae11b71cf90
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-01-04 09:15:38 -0700

    adds first part of request test

---
+++ spec/requests/giphy_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "GET /gifs?location=denver,co" do
+  it "returns time, summary and a matching gif for days weather" do
+    location = 'denver,co'
+    get "/api/v1/gifs?location=#{location}"
+
+    expect(response.status).to eq 200
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    
+  end
+end
