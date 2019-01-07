53b0a18115461e5dca6dadc9268c0fbe7b8932d8
Rajaa Boulassouak <39357526+RajaaBoulassouak@users.noreply.github.com>
2019-01-07 14:30:32 -0700

    Update favorites_controller.rb

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::FavoritesController < ApplicationController
   before_action :authorize
   
   def create
-    # render json: { message: "Unauthorized" }, status: 404 unless user
     favorite = user.favorites.create(location: params[:location])
     render json: favorite, status: 201
   end
