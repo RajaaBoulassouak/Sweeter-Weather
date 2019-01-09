b60d3b05545e57f3c32cba0a72a79600a5d40716
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-01-08 21:13:46 -0700

    adds request tests, route and destroy action for delete favorites"

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -3,14 +3,24 @@ class Api::V1::FavoritesController < ApplicationController
   before_action :authorize
   
   def index 
-    favorites = current_user.favorites.map do |fave|
-      ForecastFacade.new(fave.location).current_weather
-    end
     render json: CurrentWeatherSerializer.new(favorites)
   end
   
   def create
     favorite = current_user.favorites.create(location: params[:location])
     render json: favorite, status: 201
+  end
+  
+  def destroy 
+    Favorite.find_by(location: params[:location]).delete
+    render json: CurrentWeatherSerializer.new(favorites)
+  end
+  
+  private 
+  
+  def favorites 
+    current_user.favorites.map do |fave|
+      ForecastFacade.new(fave.location).current_weather
+    end
   end
 end
--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -54,8 +54,8 @@ end
 RSpec.describe 'GET /favorites' do 
   it 'lists favorite location' do
     user = User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password')
-    favorite_1 = user.favorites.create(location: 'Denver, CO')
-    favorite_2 = user.favorites.create(location: 'Golden, CO')
+    favorite_1 = user.favorites.create(location: 'Denver,CO')
+    favorite_2 = user.favorites.create(location: 'Golden,CO')
     params =  {
                 'api_key': "#{user.api_key}"
               }
@@ -68,8 +68,8 @@ RSpec.describe 'GET /favorites' do
     data = JSON.parse(response.body, symbolize_names: true)
 
     expect(data[:data].count).to eq(2)
-    expect(data[:data].first[:id]).to eq("Denver, CO")
-    expect(data[:data].second[:id]).to eq("Golden, CO")
+    expect(data[:data].first[:id]).to eq("Denver,CO")
+    expect(data[:data].second[:id]).to eq("Golden,CO")
     expect(data[:data].first[:type]).to eq("current_weather")
     expect(data[:data].second[:type]).to eq("current_weather")
     expect(data[:data].first[:attributes]).to have_key(:temperature)
@@ -78,8 +78,61 @@ RSpec.describe 'GET /favorites' do
   
   it 'returns status 401 (Unauthorized) if no API key is provided' do 
     user = User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password')
-    favorite_1 = user.favorites.create(location: 'Denver, CO')
-    favorite_2 = user.favorites.create(location: 'Golden, CO')
+    favorite_1 = user.favorites.create(location: 'Denver,CO')
+    favorite_2 = user.favorites.create(location: 'Golden,CO')
+    params =  {}   
+    get '/api/v1/favorites', params: params
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+    
+    data = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(data[:message]).to eq('Unauthorized')
+  end 
+  
+  it 'returns status 401 (Unauthorized) if incorrect key is provided' do 
+    user = User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password')
+    params =  {
+                'api_key': "jgn983hy-4d2b-4818-97c7-62b55ed23611"
+              }    
+    get '/api/v1/favorites', params: params
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(data[:message]).to eq('Unauthorized')
+  end
+end
+
+
+RSpec.describe 'DELETE /favorites' do 
+  it 'deletes favorites' do 
+    user = User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password')
+    favorite_1 = user.favorites.create(location: 'Denver,CO')
+    favorite_2 = user.favorites.create(location: 'Golden,CO')
+    params =  { 
+                'location': 'Denver,CO',
+                'api_key': "#{user.api_key}"
+              }    
+    delete '/api/v1/favorites', params: params
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    data = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(data[:data].count).to eq(1)
+    expect(data[:data].first[:id]).to eq("Golden,CO")
+    expect(data[:data].first[:type]).to eq("current_weather")
+    expect(data[:data].first[:attributes]).to have_key(:temperature)
+  end
+  
+  it 'returns status 401 (Unauthorized) if no API key is provided' do 
+    user = User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password')
+    favorite_1 = user.favorites.create(location: 'Denver,CO')
+    favorite_2 = user.favorites.create(location: 'Golden,CO')
     params =  {}   
     get '/api/v1/favorites', params: params
 
